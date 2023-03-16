f9633b2488723d507c22c1ef95ed977c8f56cd78
轶桐 吴 <3420675312@qq.com>
2023-03-16 11:26:25 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_catch_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            try
+            {
+                    Console.WriteLine("geben sie ihr alter ein ");
+                    age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(FormatException ex) 
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("DU HAST EIN FALSCHES FORMAT!");
+                Console.ReadLine();
+                Console.Clear();
+                
+            }
+            //catch(OverflowException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine("ZAHL ZU GROß ODER ZU KLEIN");
+            //    Console.ReadLine();
+            //    Console.Clear();
+            //}
+
+            if (age >= 18)
+            {
+                Console.WriteLine("VOLLJARIGE");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
